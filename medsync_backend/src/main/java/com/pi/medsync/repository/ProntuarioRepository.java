36976d71f3081da3114386ea7fdf28608eb026b4
Luluzera <lukebr200@gmail.com>
2025-05-14 14:38:54 -0300

    feito prontuariosPaciente,ProntuarioController e Repository

---
+++ medsync_backend/src/main/java/com/pi/medsync/repository/ProntuarioRepository.java
@@ -0,0 +1,9 @@
+package com.pi.medsync.repository;
+
+import com.pi.medsync.model.Prontuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface ProntuarioRepository extends JpaRepository<Prontuario, Long> {
+    List<Prontuario> findByPacienteIdPaciente(Long idPaciente);
+}
---
+++ medsync_backend/src/main/java/com/pi/medsync/service/ProntuarioService.java
@@ -0,0 +1,25 @@
+package com.pi.medsync.service;
+
+import com.pi.medsync.model.Prontuario;
+import com.pi.medsync.repository.ProntuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class ProntuarioService {
+
+    @Autowired
+    private ProntuarioRepository repository;
+
+    public List<Prontuario> listarPorPaciente(Long idPaciente) {
+        return repository.findByPacienteIdPaciente(idPaciente);
+    }
+
+    public Prontuario salvar(Prontuario prontuario) {
+        prontuario.setDtRegistro(LocalDateTime.now());
+        return repository.save(prontuario);
+    }
+}
