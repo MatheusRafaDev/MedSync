290d780932410c6b28c28dfe079de234e41dd1cd
Luluzera <lukebr200@gmail.com>
2025-05-14 14:38:16 -0300

    feito prontuariosPaciente,ProntuarioController e Repository

--- medsync_backend/src/main/java/com/pi/medsync/model/Prontuario.java
+++ medsync_backend/src/main/java/com/pi/medsync/model/Prontuario.java
@@ -34,4 +34,44 @@ public class Prontuario {
         this.dtRegistro = dtRegistro;
         this.dsDescricao = dsDescricao;
     }
+
+    public Long getIdProntuario() {
+        return idProntuario;
+    }
+
+    public void setIdProntuario(Long idProntuario) {
+        this.idProntuario = idProntuario;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    public Medico getMedico() {
+        return medico;
+    }
+
+    public void setMedico(Medico medico) {
+        this.medico = medico;
+    }
+
+    public LocalDateTime getDtRegistro() {
+        return dtRegistro;
+    }
+
+    public void setDtRegistro(LocalDateTime dtRegistro) {
+        this.dtRegistro = dtRegistro;
+    }
+
+    public String getDsDescricao() {
+        return dsDescricao;
+    }
+
+    public void setDsDescricao(String dsDescricao) {
+        this.dsDescricao = dsDescricao;
+    }
 }
