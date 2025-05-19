6351748450a74b1fa5df3545c6884dba72d3071c
MatheusRafaelDev <90002960+MatheusRafaDev@users.noreply.github.com>
2025-05-19 11:49:00 -0300

    colocando paciente controller

--- medsync_backend/src/main/java/com/pi/medsync/controller/PacienteController.java
+++ medsync_backend/src/main/java/com/pi/medsync/controller/PacienteController.java
@@ -1,9 +1,8 @@
 package com.pi.medsync.controller;
 
 import com.pi.medsync.model.Paciente;
-import com.pi.medsync.service.PacienteService;
+import com.pi.medsync.repository.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,18 +11,66 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/pacientes")
-@CrossOrigin
 public class PacienteController {
 
     @Autowired
-    private PacienteService pacienteService;
+    private PacienteRepository pacienteRepository;
 
-    // Criar novo paciente
+    // Criar um novo paciente
     @PostMapping
-    public ResponseEntity<Paciente> criarPaciente(@RequestBody Paciente paciente) {
-        Paciente criado = pacienteService.criarPaciente(paciente);
-        return new ResponseEntity<>(criado, HttpStatus.CREATED);
+    public ResponseEntity<Paciente> createPaciente(@RequestBody Paciente paciente) {
+        try {
+            Paciente novoPaciente = pacienteRepository.save(paciente);
+            return ResponseEntity.ok(novoPaciente);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(null);
+        }
     }
 
+    // Listar todos os pacientes
+    @GetMapping
+    public List<Paciente> getAllPacientes() {
+        return pacienteRepository.findAll();
+    }
 
+    // Obter um paciente por ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Paciente> getPacienteById(@PathVariable Long id) {
+        Optional<Paciente> paciente = pacienteRepository.findById(id);
+        return paciente.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    // Atualizar um paciente
+    @PutMapping("/{id}")
+    public ResponseEntity<Paciente> updatePaciente(@PathVariable Long id, @RequestBody Paciente pacienteDetails) {
+        Optional<Paciente> paciente = pacienteRepository.findById(id);
+
+        if (paciente.isPresent()) {
+            Paciente pacienteExistente = paciente.get();
+            pacienteExistente.setNome(pacienteDetails.getNome());
+            pacienteExistente.setCpf(pacienteDetails.getCpf());
+            pacienteExistente.setNascimento(pacienteDetails.getNascimento());
+            pacienteExistente.setTelefone(pacienteDetails.getTelefone());
+            pacienteExistente.setEndereco(pacienteDetails.getEndereco());
+            pacienteExistente.setPlanoSaude(pacienteDetails.getPlanoSaude());
+
+            Paciente updatedPaciente = pacienteRepository.save(pacienteExistente);
+            return ResponseEntity.ok(updatedPaciente);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    // Excluir um paciente
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePaciente(@PathVariable Long id) {
+        Optional<Paciente> paciente = pacienteRepository.findById(id);
+
+        if (paciente.isPresent()) {
+            pacienteRepository.delete(paciente.get());
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
